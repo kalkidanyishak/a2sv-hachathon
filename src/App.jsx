326263d048959f6dd9d5a3dbf255db55742f2143
kalkidan yishak <kalkidanyishak002@gmail.com>
2024-07-13 22:29:38 +0300

    nice work

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import MainPage from './components/mainPage/MainPage'
+
+
+function App() {
+  return (
+    <>
+    <MainPage/>
+
+    
+    </>
+  )
+}
+
+export default App
