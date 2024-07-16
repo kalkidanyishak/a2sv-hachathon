9ff7e38f768dde74dd5c99fdb112cf111463f933
Kad-19 <kidusasebe1919@gmail.com>
2024-07-17 00:34:28 +0300

    Auth and user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import MainPage from './components/mainPage/MainPage'
+import UserDashboard from './components/UserDashboard/UserDashboard'
+import Signin from './components/AuthPages/Signin'
 
 
 function App() {
   return (
     <>
-    <MainPage/>
+    <UserDashboard/>
+    {/* <Signin/> */}
 
     
     </>
